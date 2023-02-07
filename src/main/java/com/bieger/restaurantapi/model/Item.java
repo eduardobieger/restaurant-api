b12f823b5648175fbce3be2966cbc62237614af9
Eduardo Bieger <eduardo42bieger@gmail.com>
2023-02-07 08:13:36 -0300

    refactored classes

--- src/main/java/com/bieger/restaurantapi/model/Item.java
+++ src/main/java/com/bieger/restaurantapi/model/Item.java
@@ -1,14 +1,88 @@
 package com.bieger.restaurantapi.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
-@MappedSuperclass
-public abstract class Item {
+@Entity
+@Table(name = "item")
+public class Item {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "title", nullable = false)
 	private String title;
+	
+	@Column(name = "price", nullable = false)
 	private BigDecimal price;
+	
+	@Column(name = "description")
+	private String description;
+	
+//	@OneToMany(mappedBy = "items", cascade = CascadeType.ALL)
+//	private List<Order> orders;
+	
+	public Item() {
+		
+	}
+
+	public Item(Integer id, String title, BigDecimal price, String description) {
+		this.id = id;
+		this.title = title;
+		this.price = price;
+		this.description = description;
+//		this.orders = orders;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public BigDecimal getPrice() {
+		return price;
+	}
+	
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+//	public List<Order> getOrders() {
+//		return orders;
+//	}
+//	
+//	public void setOrders(List<Order> orders) {
+//		this.orders = orders;
+//	}
 	
 }
--- src/main/java/com/bieger/restaurantapi/model/User.java
+++ src/main/java/com/bieger/restaurantapi/model/User.java
@@ -1,36 +1,125 @@
 package com.bieger.restaurantapi.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
-@MappedSuperclass
-public abstract class User {
+@Entity
+@Table(name = "user")
+public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "username", nullable = false)
 	private String username;
+	
+	@Column(name = "password", nullable = false)
 	private String password;
 	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+	
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+	@Column(name = "email")
+	private String email;
+	
+//	@OneToMany(mappedBy = "orderUser", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//	private List<Order> orders;
+	
+	public User() {
+		
+	}
+
+	public User(Integer id, String username, String password, String firstName, String lastName, String phoneNumber,
+			String email) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+//		this.orders = orders;
+	}
+
 	public Integer getId() {
 		return id;
 	}
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+//	public List<Order> getOrders() {
+//		return orders;
+//	}
+//	
+//	public void setOrders(List<Order> orders) {
+//		this.orders = orders;
+//	}
 	
 }
